3fa5f188deea0c434be4f44a67ea16956cdaca46
Christian I. Bwendo <christianbwendo@gmail.com>
2025-10-27 12:13:22 +0200

    Deploy Flutter web app

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "67bccad6ca1dec670a0f759acde1040d
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "73bdbfa739372adb173d83d9f5d08901",
+"flutter_bootstrap.js": "f484584eaacf28a084a63fe458c691e5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
